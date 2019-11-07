7a52ef109da5a825a5e7d4568fd5361f3523b3c8
Tracy <tmullen@clearblade.com>
2019-11-07 07:50:49 -0600

    seperate files

---
+++ script.js
@@ -0,0 +1,73 @@
+function renderGroupImages({ grouped, group }) {
+  window.group = group;
+  $(".thumb-gallery").html("");
+  $(".full-images").html(
+    grouped[group]
+      .map(imgStr => {
+        return `<img class="full-img" id="${imgStr}" src="https://res.cloudinary.com/spiffysparrow/image/upload//w_1200/${imgStr}" />`;
+      })
+      .join("")
+  );
+}
+
+function renderThumbs(grouped) {
+  $(".full-images").html("");
+  Object.keys(grouped).forEach(groupName => {
+    const group = grouped[groupName];
+    $(".thumb-gallery").prepend(`
+          <div class="thumb">
+            <a href="#ex1" rel="modal:open">
+                <img class="thumb-img" data-group="${groupName}" id="${
+      group[0]
+    }" src="https://res.cloudinary.com/spiffysparrow/image/upload//w_500/${
+      group[0]
+    }" />
+            </a>
+          </div>
+        `);
+  });
+}
+
+$.ajax(
+  "https://res.cloudinary.com/spiffysparrow/image/list/cardboard.json"
+).then(({ resources }) => {
+  const grouped = resources.reduce((acc, d) => {
+    var arr = d.public_id.split("/");
+    acc[arr[1]] = acc[arr[1]] || [];
+    acc[arr[1]].push(d.public_id + "." + d.format);
+    return acc;
+  }, {});
+
+  const groupKeys = Object.keys(grouped);
+
+  renderThumbs(grouped);
+
+  $("body").on("click", ".thumb-img", e => {
+    var group = e.currentTarget.dataset.group;
+    renderGroupImages({ grouped, group });
+  });
+
+  $(".home-btn").click(() => {
+    renderThumbs(grouped);
+  });
+  $(".prev-btn").click(() => {
+    var i = groupKeys.indexOf(window.group);
+    i--;
+    if (i < 0) {
+      i = groupKeys.length - 1;
+    }
+    var newGroup = groupKeys[i];
+
+    renderGroupImages({ grouped, group: newGroup });
+  });
+  $(".next-btn").click(() => {
+    var i = groupKeys.indexOf(window.group);
+    i++;
+    if (i > groupKeys.length - 1) {
+      i = 0;
+    }
+    var newGroup = groupKeys[i];
+
+    renderGroupImages({ grouped, group: newGroup });
+  });
+});
